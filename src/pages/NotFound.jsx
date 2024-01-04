f2327aae91c93ac4d2db38c84029eeceaa69d470
grace <grace994084@gmail.com>
2024-01-04 17:52:56 +0900

    feat : page not found component 구현

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,18 @@
+import Button from '@components/common/Button';
+import { Wrapper, TextBox, ButtonBox, H1, P } from '@styles/NotFound.style';
+
+function NotFound() {
+  return (
+    <Wrapper>
+      <TextBox>
+        <H1>404</H1>
+        <P>Page Not Found</P>
+      </TextBox>
+      <ButtonBox>
+        <Button $size="medium">HOME</Button>
+      </ButtonBox>
+    </Wrapper>
+  );
+}
+
+export default NotFound;
---
+++ src/styles/NotFound.style.js
@@ -0,0 +1,29 @@
+import { styled } from 'styled-components';
+
+export const Wrapper = styled.div`
+  padding-top: 280px;
+`;
+
+export const TextBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+
+export const H1 = styled.h1`
+  font-size: 64px;
+  color: #66cdaa;
+`;
+
+export const P = styled.p`
+  font-size: 32px;
+  font-weight: bold;
+  color: #66cdaa;
+`;
+
+export const ButtonBox = styled.div`
+  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+`;
